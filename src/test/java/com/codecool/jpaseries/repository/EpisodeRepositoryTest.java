670d7c6280ba607de9de30d49d5d072f11749347
Adam Sallai <yadam1998@gmail.com>
2020-11-09 18:02:01 +0100

    add test for saving episodes with the same unique field throws error as expected

--- src/test/java/com/codecool/jpaseries/repository/EpisodeRepositoryTest.java
+++ src/test/java/com/codecool/jpaseries/repository/EpisodeRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -33,5 +34,24 @@ public class EpisodeRepositoryTest {
         episodeRepository.save(episode);
 
         assertEquals(episode.getTitle(), episodeRepository.findAll().get(0).getTitle());
+    }
+
+    @Test
+    public void saveUniqueFieldTwiceThrowsDataIntegrityViolationException() {
+        Episode episode1 = Episode.builder()
+                .episodeNumber(1)
+                .title("Example episode one")
+                .build();
+        Episode episode2 = Episode.builder()
+                .episodeNumber(1)
+                .title("Example episode two")
+                .build();
+
+        episodeRepository.save(episode1);
+
+        assertThrows(DataIntegrityViolationException.class, () -> {
+            episodeRepository.saveAndFlush(episode2);
+        });
+
     }
 }
